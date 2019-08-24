780d947097d53ad4cd466303f984961a4b92543b
Austin Robbins <austinrobbins@Austins-MBP.hsd1.wa.comcast.net>
2019-08-23 22:39:41 -0700

    need to add survey and routes

---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const htmlRouter = require('./app/routing/htmlRoutes')
+const apiRouter = require('./app/routing/apiRoutes')
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/', htmlRouter)
+app.use('/api', apiRouter)
+
+
+//testing
+app.listen(PORT, (err) => {
+    if (err) return console.log(err);
+    console.log(`APP LISTENING ON PORT: ${PORT}`);
+})
