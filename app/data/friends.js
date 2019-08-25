6dc4b62acbdb8ffef42af4c464909504557c204d
Austin Robbins <austinrobbins@Austins-MBP.hsd1.wa.comcast.net>
2019-08-24 17:58:37 -0700

    survey added - routing from home to survey complete. Need to write js for survey to interact with selections + need to add proper routing

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+const friends = [
+    {
+        name: "pi",
+        photo: "images/pi.png",
+        scores: "1,2,3,4,5,1,2,3,4,5",
+    },
+    {
+        name: "ka",
+        photo: "images/ka.png",
+        scores: "5,4,4,5,4,5,4,3,2,1",
+    },
+    {
+        name: "chu",
+        photo: "images/chu.png",
+        scores: "5,5,5,5,5,5,5,5,5,5"
+    }
+]
+ module.exports = friends;
